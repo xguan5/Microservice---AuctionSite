ddbb8d0c239cefb0dc682ca49ed391291a211d00
Chip Locke <chip@Chips-MacBook-Pro.local>
2020-11-09 09:22:06 -0600

    tweaking readme

--- frontend-service/app/user_client.py
+++ frontend-service/app/user_client.py
@@ -4,7 +4,7 @@ import requests
 
 def get_user_list():
 
-    user_service_ip = "127.20.0.1"
+    user_service_ip = "172.20.0.1"
     url = 'http://{}:5090/api/user_list'.format(user_service_ip)
     response = requests.get(url=url)
 
--- user-service/app/user_client.py
+++ user-service/app/user_client.py
@@ -9,5 +9,15 @@ def get_user_list():
             'username': 'Tester',
             'description': 'Filler Texfeaft',
              'rating': 4.0
+        },
+        {
+            'username': 'Again',
+            'description': 'The ith tije acfioj aoi',
+             'rating': 3.0
+        },
+        {
+            'username': 'Tester',
+            'description': 'Filler Texfeaft',
+             'rating': 5.0
         }
     ]
