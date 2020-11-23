842b836c837cd26aebfea82ffa97427d40ec9fb3
Xiangshi Guan <xguan2@cs.uchicago.edu>
2020-11-22 21:34:06 -0600

    updated item-service

--- item-service/app/models.py
+++ item-service/app/models.py
@@ -21,66 +21,54 @@ def create_tables(app):
     db.metadata.create_all(engine)
     return engine
 
-class Auction(db.Model):
+class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255),unique = False, nullable = True)
-    buy_now_price = db.Column(db.Float)
-    start_bid_price = db.Column(db.Float)
-    inc_bid_price = db.Column(db.Float)
-    status = db.Column(db.String(50), default="Draft")
-    start_time = db.Column(db.DateTime, default=datetime.utcnow)
-    end_time = db.Column(db.DateTime, default=datetime.utcnow)
-    date_updated = db.Column(db.DateTime, onupdate = datetime.utcnow)
-    creator_id = db.Column(db.Integer)
-    manager_id = db.Column(db.Integer)
-    winner_id = db.Column(db.Integer)
-    item_id = db.Column(db.Integer)
-    biddings = db.relationship('Bidding')
+    description = db.Column(db.String(255),unique = False, nullable = True)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    flag_id = db.Column(db.Integer, db.ForeignKey('flag.id'))
 
 
-
-    def __init__(self,name,buy_now_price,start_bid_price,inc_bid_price,start_time,end_time,creator_id,item_id):
+    def __init__(self,name,description):
         self.name = name
-        self.buy_now_price = buy_now_price
-        self.start_bid_price = start_bid_price
-        self.inc_bid_price = inc_bid_price
-        self.start_time = start_time
-        self.end_time = end_time
-        self.creator_id = creator_id
-        self.item_id = item_id
+        self.description = description
 
     def to_json(self):
         return {
             'name': self.name,
-            'creator': self.creator_id,
-            'item': self.item_id,
-            'buy_now_price': self.buy_now_price,
-            'start_bid_price': self.start_bid_price,
-            'inc_bid_price': self.inc_bid_price,
-            'start_time': self.start_time,
-            'end_time': self.end_time,
-            'status': self.status,
-            'date_updated': self.date_updated,
-            'winner': self.winner_id
+            'description': self.description,
+            'category': self.category_id,
+            'flag': self.flag_id
+
         }
 
-class Bidding(db.Model):
+class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer)
-    auction_id = db.Column(db.Integer, db.ForeignKey('auction.id') )
-    bid_price = db.Column(db.Float)
-    bid_placed = db.Column(db.DateTime, default=datetime.utcnow)
+    name = db.Column(db.String(255),unique = False, nullable = True)
+    items = db.relationship('Item')
 
-    def __init__(self,user_id,auction_id,bid_price,bid_placed):
-        self.user_id = user_id
-        self.auction_id = auction_id
-        self.bid_price = bid_price
-        self.bid_placed = bid_placed
+    def __init__(self,name):
+        self.name = name
+
 
     def to_json(self):
         return {
-            'user': self.user_id,
-            'auction': self.auction_id,
-            'bid_price': self.bid_price,
-            'bid_placed': self.bid_placed
+            'name': self.name,
         }
+
+class Flag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255),unique = False, nullable = True)
+    items = db.relationship('Item')
+
+    def __init__(self,name):
+        self.name = name
+
+
+    def to_json(self):
+        return {
+            'name': self.name,
+        }
+
+
+
--- item-service/app/routes.py
+++ item-service/app/routes.py
@@ -11,196 +11,187 @@ from pandas.io.json import json_normalize
 
 bp = Blueprint('routes', __name__, url_prefix='/')
 
-#get all auctions
-@bp.route('/api/auctions', methods=['GET'])
-def get_auctions():
-	all_auctions = []
-	for row in models.Auction.query.all():
-		all_auctions.append(row.to_json())
-	res = jsonify(all_auctions)
+#get all items
+@bp.route('/api/items', methods=['GET'])
+def get_items():
+	all_items = []
+	for row in models.Item.query.all():
+		all_items.append(row.to_json())
+	res = jsonify(all_items)
 	return res
 
-#get one auction
-@bp.route('/api/auction/<id>',methods = ['GET'])
-def get_auction(id):
-	auction = models.Auction.query.get(id)
-	return jsonify({'result': auction.to_json()})
+#get one item
+@bp.route('/api/item/<id>',methods = ['GET'])
+def get_item(id):
+	item = models.Item.query.get(id)
+	return jsonify({'result': item.to_json()})
 
-#create a new auction
-@bp.route('api/auction/create',methods=['POST'])
-def create_auction():
+#create a new item
+@bp.route('api/item/create',methods=['POST'])
+def create_item():
 	#content = request.get_json(force=True)
 	content = request.form
 	name = content['name']
-	buy_now_price = content['buy_now_price']
-	start_bid_price = content['start_bid_price']
-	inc_bid_price = content['inc_bid_price']
-	start_time = content['start_time']
-	end_time = content['end_time']
-	creator = content['creator']
-	item = content['item']
+	description = content['description']
+	if 'category' in content.keys():
+		category = content['category']
+	if 'flag' in content.keys():
+		flag = content['flag']
 
-	new_auction = models.Auction(name,buy_now_price,start_bid_price,inc_bid_price,start_time,end_time,creator,item)
+	new_item = models.Item(name,description)
 
-	models.db.session.add(new_auction)
+	models.db.session.add(new_item)
 	models.db.session.commit()
 
-	return jsonify({'result': new_auction.to_json()})
+	return jsonify({'result': new_item.to_json()})
 
 
-#update a auction, use this to mark end and start of auction by setting status
-@bp.route('/api/auction/<id>',methods=['PUT'])
-def update_auction(id):
-	auction = models.Auction.query.get(id)
+#update a item, add item to a category or flag
+@bp.route('/api/item/<id>',methods=['PUT'])
+def update_item(id):
+	item = models.Item.query.get(id)
 
 	content = request.form
 	if 'name' in content.keys():
 		name = content['name']
-		auction.name = name
-	if 'buy_now_price' in content.keys():
-		buy_now_price = content['buy_now_price']
-		auction.buy_now_price = buy_now_price
-	if 'start_bid_price' in content.keys():
-		start_bid_price = content['start_bid_price']
-		auction.start_bid_price = start_bid_price
-	if 'inc_bid_price' in content.keys():
-		inc_bid_price = content['inc_bid_price']
-		auction.inc_bid_price = inc_bid_price
-	if 'start_time' in content.keys():
-		start_time = content['start_time']
-		auction.start_time = start_time
-	if 'end_time' in content.keys():
-		end_time = content['end_time']
-		auction.end_time = end_time
-	if 'creator' in content.keys():
-		creator = content['creator']
-		auction.creator = creator
-	if 'item' in content.keys():
-		item = content['item']
-		auction.item = item
-	if 'status' in content.keys():
-		status = content['status']
-		auction.status = status
+		item.name = name
+	if 'description' in content.keys():
+		description = content['description']
+		item.description = description
+	if 'category' in content.keys():
+		category = content['category']
+		item.category_id = category
+	if 'flag' in content.keys():
+		flag = content['flag']
+		item.flag_id = flag
 
 	models.db.session.commit()
 
-	return jsonify({'result': auction.to_json()})
+	return jsonify({'result': item.to_json()})
 
-#delete auction
-@bp.route('/api/auction/<id>',methods=['DELETE'])
-def delete_auction(id):
-	auction = models.Auction.query.get(id)
-	models.db.session.delete(auction)
+#delete item
+@bp.route('/api/item/<id>',methods=['DELETE'])
+def delete_item(id):
+	item = models.Item.query.get(id)
+	models.db.session.delete(item)
 	models.db.session.commit()
-	return jsonify({'result': auction.to_json()})
+	return jsonify({'result': item.to_json()})
 
-
-#get all biddings for a given auction
-@bp.route('/api/auction/<id>/bids', methods=['GET'])
-def get_bids(id):
-	#auction = models.Auction.query.get(id)
-	all_biddings = []
-	for row in models.Bidding.query.filter_by(auction_id=id):
-		all_biddings.append(row.to_json())
-	res = jsonify(all_biddings)
-	return res
-
-#return the highest bid so far
-@bp.route('/api/auction/<id>/highestbid', methods=['GET'])
-def highest_bid(id):
-	all_bids = json.loads(get_bids(id).data.decode('utf-8').replace("'", '"'))
-	#all_bids = json.dumps(raw_bids, indent=4, sort_keys=True)
-	max_bid = 0
-	for it in all_bids:
-		if it['bid_price'] > max_bid:
-			max_bid = it['bid_price']
-	return jsonify({'max_bid':max_bid})
-
-#return the start bid price of an auction
-@bp.route('/api/auction/<id>/startbid',methods = ['GET'])
-def start_bid(id):
-	auction = models.Auction.query.get(id)
-	res = jsonify(auction.start_bid_price)
-	return res
-
-#return the incremental bid price of an auction
-@bp.route('/api/auction/<id>/incbid',methods = ['GET'])
-def inc_bid(id):
-	auction = models.Auction.query.get(id)
-	res = jsonify(auction.inc_bid_price)
-	return res
-
-#add bidding to auction
-@bp.route('api/auction/<id>/createbid',methods=['POST'])
-def create_bid(id):
+#create a category
+@bp.route('api/category/create',methods=['POST'])
+def create_category():
+	#content = request.get_json(force=True)
 	content = request.form
-	
-	bidder = content['user_id']
-	bid_price = content['bid_price']
-	bid_placed = content['bid_placed']
+	name = content['name']
 
-	#check new bid is higher than existing
-	#to do: check if higher than starting bid,higher by inc_bid
-	cur_high_bid = json.loads(highest_bid(id).data.decode('utf-8').replace("'", '"'))['max_bid']
-	starting_bid = json.loads(start_bid(id).data.decode('utf-8').replace("'", '"'))
-	incremental_bid = json.loads(inc_bid(id).data.decode('utf-8').replace("'", '"'))
-	
-	auction = models.Auction.query.get(id)
-	if auction.end_time < datetime.now() or auction.status == 'end':
-		success = False
-		return {'result':success,'content':'Auction is already over, cannot place new bid'}
-	if int(bid_price) < int(cur_high_bid):
-		#raise value error or jsonify in 'status: error'
-		success = False
-		return {'result':success,'content':'new bid must be higher than existing bids'}
-	elif int(bid_price) < int(starting_bid):
-		success = False
-		return {'result':success,'content':'new bid must be higher than starting bid price'}
-	elif int(bid_price) - int(cur_high_bid) < int(incremental_bid) or int(bid_price) - int(starting_bid) < int(incremental_bid):
-		success = False
-		return {'result':success,'content':'new bid must be higher than existing bids at least by the incremental amount'}	
-	else:
-		new_bid = models.Bidding(bidder,id,bid_price,bid_placed)
+	new_category = models.Category(name)
 
-		models.db.session.add(new_bid)
-		models.db.session.commit()
+	models.db.session.add(new_category)
+	models.db.session.commit()
 
-		success = True
-		return jsonify({'result': success, ' content': new_bid.to_json()})
+	return jsonify({'result': new_category.to_json()})
 
-#no need for update bid and delete bid, since we won't allow user to do that
+#delete a category
+#To do, after delete this category, what happen to the items in this category?
+@bp.route('/api/category/<id>',methods=['DELETE'])
+def delete_category(id):
+	category = models.Category.query.get(id)
+	models.db.session.delete(category)
+	models.db.session.commit()
+	return jsonify({'result': category.to_json()})
 
-#check if auction status
-@bp.route('/api/auction/<id>/status',methods = ['GET'])
-def get_status(id):
-	auction = models.Auction.query.get(id)
-	res = jsonify(auction.status)
+#update a category
+@bp.route('/api/category/<id>',methods=['PUT'])
+def update_category(id):
+	category = models.Category.query.get(id)
+
+	content = request.form
+	if 'name' in content.keys():
+		name = content['name']
+		item.name = name
+
+	models.db.session.commit()
+
+	return jsonify({'result': category.to_json()})
+
+#get all categories
+@bp.route('/api/categories', methods=['GET'])
+def get_categories():
+	all_categories = []
+	for row in models.Category.query.all():
+		all_categories.append(row.to_json())
+	res = jsonify(all_categories)
+	return res
+
+#get all items for a given category
+@bp.route('/api/category/<id>/items', methods=['GET'])
+def get_category_items(id):
+	category = models.Category.query.get(id)
+	all_items = []
+	for row in category.items:
+		all_items.append(row.to_json())
+	res = jsonify(all_items)
+	return res
+
+#delete an item from a category just use update item function and update category to null
+
+
+
+#create a flag
+@bp.route('api/flag/create',methods=['POST'])
+def create_flag():
+	#content = request.get_json(force=True)
+	content = request.form
+	name = content['name']
+
+	new_flag = models.Flag(name)
+
+	models.db.session.add(new_flag)
+	models.db.session.commit()
+
+	return jsonify({'result': new_flag.to_json()})
+
+#update a flag
+@bp.route('/api/flag/<id>',methods=['PUT'])
+def update_flag(id):
+	flag = models.Flag.query.get(id)
+
+	content = request.form
+	if 'name' in content.keys():
+		name = content['name']
+		item.name = name
+
+	models.db.session.commit()
+
+	return jsonify({'result': flag.to_json()})
+
+#delete a flag
+@bp.route('/api/flag/<id>',methods=['DELETE'])
+def delete_flag(id):
+	flag = models.Flag.query.get(id)
+	models.db.session.delete(flag)
+	models.db.session.commit()
+	return jsonify({'result': flag.to_json()})
+
+#get all flags
+@bp.route('/api/flags', methods=['GET'])
+def get_flags():
+	all_flags = []
+	for row in models.Flag.query.all():
+		all_flags.append(row.to_json())
+	res = jsonify(all_flags)
+	return res
+#get all items for a given flag
+@bp.route('/api/flag/<id>/items', methods=['GET'])
+def get_flag_items(id):
+	flag = models.Flag.query.get(id)
+	all_items = []
+	for row in flag.items:
+		all_items.append(row.to_json())
+	res = jsonify(all_items)
 	return res
 
 
-
-#find winner of an auction
-#front end udpate auction with winner
-@bp.route('/api/auction/<id>/winner',methods = ['GET'])
-def find_auction_winner(id):
-	#need to check if auction is over
-	auction = models.Auction.query.get(id)
-	if datetime.now() > auction.end_time or auction.status != 'end':
-		high_bid = json.loads(highest_bid(id).data.decode('utf-8').replace("'", '"'))['max_bid']
-		win_bid = models.Bidding.query.filter_by(bid_price = high_bid).first()
-		win_bidder = win_bid.to_json()['user']
-		success = True
-		return jsonify({'result': success, 'content': win_bidder})
-	else:
-		success = False
-		return jsonify({'result': success, 'content': "Auction is not over yet"})
-
-
-#get winner of an auction
-@bp.route('/api/auction/<id>/winner',methods = ['GET'])
-def get_auction_winner(id):
-	#need to check if auction is over
-	auction = models.Auction.query.get(id)
-	return jsonify({'result': auction.winner})
+##delete an item from a flag just use update item function and update flag to null
 
 
