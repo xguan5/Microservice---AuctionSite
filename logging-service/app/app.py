821167d864e26389f88cac0d753db908bd03f118
Xiangshi Guan <xguan2@cs.uchicago.edu>
2020-12-02 21:08:49 -0600

    add rabbit mq

--- logging-service/app/app.py
+++ logging-service/app/app.py
@@ -1,6 +1,7 @@
 import json
 from flask import Flask, request, jsonify
 from flask_mongoengine import MongoEngine
+import pika
 
 app = Flask(__name__)
 app.config['MONGODB_SETTINGS'] = {
@@ -10,6 +11,16 @@ app.config['MONGODB_SETTINGS'] = {
 }
 db = MongoEngine()
 db.init_app(app)
+
+#set up receiver end of rabbitmq
+credentials = pika.PlainCredentials(username='guest', password='guest')
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost',port=5672,credentials=credentials))
+
+channel = connection.channel()
+
+channel.queue_declare(queue='hello')
+
+
 
 class Logs(db.Document):
     service = db.StringField()
@@ -22,6 +33,7 @@ class Logs(db.Document):
                 "action": self.action,
                 "content": self.content}
 
+
 @app.route('/', methods=['GET'])
 def query_records():
     service = request.args.get('service')
@@ -31,17 +43,40 @@ def query_records():
     else:
         return jsonify(log.to_json())
 
+#don't need this any more
 @app.route('/api/create_log', methods=['POST'])
-def create_log():
+def create_log(record):
 
     #record = json.loads(request.form)
-    record = request.form
-
+    #record = request.form
+    print('record is ', record)
     log = Logs(service=record['service'],timestamp=record['timestamp'],action=record['action'],content=record['content'])
     log.save()
     return jsonify(log.to_json())
 
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body)
+    record = json.loads(body)
+    log = Logs(service=record['service'],timestamp=record['timestamp'],action=record['action'],content=record['content'])
+    log.save()
+
+channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+
+print(' [*] Waiting for messages. To exit press CTRL+C')
+channel.start_consuming()
+
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    try:
+        app.run(debug=True)
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
+
+
+
+
