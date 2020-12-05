1e3151eae0a06edbbb3e4eefb475a2e72d9fc702
Michelle (Xiao) Huang <michelle.huang@chicagobooth.edu>
2020-12-05 01:42:08 -0600

    minor changes, with issue
    could not connect to server: Connection refused (0x0000274D/10061)

--- payment-service/app/app.py
+++ payment-service/app/app.py
@@ -8,7 +8,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
 
     app.config.from_mapping(
-        SECRET_KEY='pmtsk'
+        SECRET_KEY='pmtsk',
         SQLALCHEMY_DATABASE_URI='postgresql://postgres:postgres@localhost/payment_db'
     )
 
